85fea7fd268abd6997aecf72e60602c4e3bf221f
Simon Guest <ssguest@theysay.io>
2016-09-07 16:03:27 +0100

    Minor tweaks following suggestions to getting_started.sh

--- preceive-cli/src/dist/bin/getting_started.sh
+++ preceive-cli/src/dist/bin/getting_started.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
+
+BINDIR=$(dirname $(readlink -f $0))
 # Input data to be submitted to PreCeive REST API
-INPUT=corpus/getting_started.json
+INPUT=${BINDIR}/../corpus/getting_started.json
 
 # File for the responses received
 OUTPUT=results/getting_started_responses.json
 
 # Your PreCeive REST API credentials
-USR=...
-PWD=...
+USERNAME=...
+PASSWORD=...
+
+if [ "${USERNAME}" == "..." ]
+   then
+     printf "\nPlease edit $0 to provide username and password for PreCeive API\n\n"
+     exit 1
+fi
 
 # The PreCeive REST API end points to call
+# <field in json object>=<api endpoint>?level=sentence
+
 ENDPOINTS="sentiment.sentence=/v1/sentiment?level=sentence emotion.sentence=/v1/emotion?level=sentence"
 
 echo "$INPUT -> $OUTPUT"
 
-./bin/preceive-cli \
-   -user ${USR} \
-   -password ${PWD} \
+${BINDIR}/preceive-cli \
+   -user ${USERNAME} \
+   -password ${PASSWORD} \
    -endpoints ${ENDPOINTS} \
    -output ${OUTPUT} \
    -batch ${INPUT}
